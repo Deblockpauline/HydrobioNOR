f597cf46a48efad3a484a9ad0b19d5a3f62d4bc8
Deblockpauline <pauline.deblock@ofb.gouv.fr>
2026-05-11 13:41:18 +0200

    Ajout ref

--- dev/prep_data_ref.R
+++ dev/prep_data_ref.R
@@ -1,7 +1,7 @@
 # Departement chemin : "\\ad.intra\dfs\COMMUNS\REGIONS\nor\DR\OFB\SIG\DR\REFERENTIEL\BDCARTO\BDC_4-0_SHP_LAMB93_R28-ED211\ADMINISTRATIF\DEPARTEMENT.shp"
 #region chemin: "\\ad.intra\dfs\COMMUNS\REGIONS\nor\DR\OFB\SIG\DR\REFERENTIEL\BDCARTO\BDC_4-0_SHP_LAMB93_R28-ED211\ADMINISTRATIF\REGION.shp"
 #bassin hydro : "\\ad.intra\dfs\COMMUNS\REGIONS\nor\DR\OFB\SIG\DR\REFERENTIEL\BD TOPAGE\NOR_BassinHydrographique_FXX.gpkg"
-
+# Bassins versants : "C:/Users/pauline.deblock/Documents/stage Pauline/R/hydrobioNOR/données/couche BV/BV_NOR.shp"
 #mettre les limites de region de NOR
 limites_region <-  sf::st_read("//ad.intra/dfs/COMMUNS/REGIONS/nor/DR/OFB/SIG/DR/REFERENTIEL/BDCARTO/BDC_4-0_SHP_LAMB93_R28-ED211/ADMINISTRATIF/DEPARTEMENT.shp") %>%
   dplyr::filter(INSEE_REG == "28") %>% #filtre de normandie
@@ -14,12 +14,29 @@ limites_region_l <- limites_region %>%
 
 # Faire les limites des bassins, meme fonctionnement que region
 limites_cours_eau <- sf::st_read(
-  dsn ="//ad.intra/dfs/COMMUNS/REGIONS/nor/DR/OFB/SIG/DR/REFERENTIEL/BD TOPAGE/NOR_CoursEau_FXX.gpkg"
-) %>%
+  dsn ="//ad.intra/dfs/COMMUNS/REGIONS/nor/DR/OFB/SIG/DR/REFERENTIEL/BD TOPAGE/NOR_CoursEau_FXX.gpkg") %>%
   sf::st_transform(crs = 4326) %>%
   rmapshaper::ms_simplify()
 
+#### Bassins versants ####
+limites_bv <- sf::st_read(
+  "C:/Users/pauline.deblock/Documents/stage Pauline/R/hydrobioNOR/données/couche BV/BV_NOR.shp",
+  quiet = TRUE) %>%
+  sf::st_make_valid() %>%                # Sécurise les géométries si besoin
+  sf::st_transform(crs = 4326) %>%       # Projection compatible leaflet
+  rmapshaper::ms_simplify(keep = 0.08)   # Allège un peu la couche
+
+# On garde uniquement les contours des bassins versants pour ne pas surcharger la carte
+limites_bv_l <- limites_bv %>%
+  sf::st_cast("MULTILINESTRING")
+
+
+#### Enregistrement dans les données internes du package ####
 usethis::use_data(
-  limites_region, limites_region_l,
+  limites_region,
+  limites_region_l,
   limites_cours_eau,
-  internal = TRUE, overwrite = TRUE)
+  limites_bv,
+  limites_bv_l,
+  internal = TRUE,
+  overwrite = TRUE)
